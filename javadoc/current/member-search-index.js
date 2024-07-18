11e8f209e2c6ab4c68536ec4e940136ac4b1b4aa
GitHub Action <action@github.com>
2024-07-18 09:09:26 +0000

    chore: updates index.html files incl. javadoc versions

--- javadoc/current/member-search-index.js
+++ javadoc/current/member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"dev.thriving.oss.java.library.template","c":"JavaLibraryTemplate","l":"JavaLibraryTemplate()","u":"%3Cinit%3E()"},{"p":"dev.thriving.oss.java.library.template","c":"JavaLibraryTemplate","l":"toFunkyCase(String)","u":"toFunkyCase(java.lang.String)"}];updateSearchResults();
+memberSearchIndex = [{"p":"com.tonyghouse.shabbo.lib","c":"JavaLibraryTemplate","l":"JavaLibraryTemplate()","u":"%3Cinit%3E()"},{"p":"com.tonyghouse.shabbo.lib","c":"JavaLibraryTemplate","l":"toFunkyCase(String)","u":"toFunkyCase(java.lang.String)"}];updateSearchResults();
--- javadoc/current/type-search-index.js
+++ javadoc/current/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"dev.thriving.oss.java.library.template","l":"JavaLibraryTemplate"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.tonyghouse.shabbo.lib","l":"JavaLibraryTemplate"}];updateSearchResults();
